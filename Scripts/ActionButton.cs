a537311e7292c9397ba6611173ce6ebbef817daf
jmickle66666666 <jaymickle@gmail.com>
2020-03-16 17:53:07 +0100

    Create ActionButton.cs

---
+++ Scripts/ActionButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ActionButton : MonoBehaviour
+{
+    public UnityEvent action;
+
+    public void Run()
+    {
+        action.Invoke();
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ActionButton))]
+public class ActionButtonInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var actionButton = (target as ActionButton);
+        if (GUILayout.Button("Run"))
+        {
+            actionButton.Run();
+        }
+    }
+}
+#endif
