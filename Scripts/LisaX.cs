69341fa7e0c108fc98e8f2086ebba87ab0bd203f
jmickle66666666 <jaymickle@gmail.com>
2020-04-23 17:10:56 +0200

    LisaX: Error handling, method arguments

--- Scripts/LisaX.cs
+++ Scripts/LisaX.cs
@@ -2,6 +2,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+public enum LisaXParameter
+{
+    Label,
+    Variable,
+    Value,
+    String,
+    Float
+}
+
 public struct LisaXMethodHook
 {
     public string name;
@@ -9,6 +18,8 @@ public struct LisaXMethodHook
 
     public System.Action<string[]> method;
     public bool continues;
+
+    public LisaXParameter[] parameters;
 }
 
 public class LisaX
@@ -22,109 +33,192 @@ public class LisaX
     Stack<(string label, int line)> callStack;
     (string label, int line) currentPosition;
 
+    static string errorFilePath = "it broke.txt";
+
     public LisaX()
     {
         methodHooks = new Dictionary<string, LisaXMethodHook>();
         callStack = new Stack<(string, int)>();
 
         // Built-in methods
-        AddMethodHook("end", () => {}, false, "Stops execution! Scripts will already stop when they run out of commands but this can be used to add clarity or add a breakpoint.");
-        AddMethodHook("inc", (data) => {
-            if (properties.ContainsKey(data[1])) {
-                properties[data[1]] = (float.Parse(properties[data[1]]) + 1).ToString();
-            } else {
-                properties.Add(data[1], "1");
-            }
-        }, "Usage: `inc [value]`\nIncreases a value by one. Useful for counters or loops. Equivalent to `add val 1`. If value doesn't exist, it is created and set to 1.");
-        AddMethodHook("dec", (data) => {
-            if (properties.ContainsKey(data[1])) {
-                properties[data[1]] = (float.Parse(properties[data[1]]) - 1).ToString();
-            } else {
-                properties.Add(data[1], "-1");
-            }
-        }, "Usage: `dec [value]`\nDecreases a value by one. Useful for counters or loops. Equivalent to `sub val 1`. If value doesn't exist, it is created and set to -1.");
-        AddMethodHook("add", (data) => {
-            if (properties.ContainsKey(data[1])) {
-                properties[data[1]] = (float.Parse(properties[data[1]]) + float.Parse(data[2])).ToString();
-            } else {
-                properties.Add(data[1], float.Parse(data[2]).ToString());
-            }
-        }, "Usage: `add [value] [amount]`\nAdds amount to value. If value doesn't exist, it is created and set to [amount].");
-        AddMethodHook("sub", (data) => {
-            if (properties.ContainsKey(data[1])) {
-                properties[data[1]] = (float.Parse(properties[data[1]]) - float.Parse(data[2])).ToString();
-            } else {
-                properties.Add(data[1], (-float.Parse(data[2])).ToString());
-            }
-        }, "Usage: `sub [value] [amount]`\nSubtracts amount from value. If value doesn't exist, it is created and set to -[amount].");
-        AddMethodHook("mul", (data) => {
-            properties[data[1]] = (float.Parse(properties[data[1]]) * float.Parse(data[2])).ToString();
-        }, "Usage: `mul [value] [amount]`");
-        AddMethodHook("div", (data) => {
-            properties[data[1]] = (float.Parse(properties[data[1]]) / float.Parse(data[2])).ToString();
-        }, "Usage: `div [value] [amount]`");
-        AddMethodHook("pow", (data) => {
-            properties[data[1]] = Mathf.Pow(float.Parse(properties[data[1]]), float.Parse(data[2])).ToString();
-        }, "Usage: `pow [value] [amount]`");
-        AddMethodHook("goto", (data) => {
-            callStack.Push(currentPosition);
-            RunLabel(data[1]);
-        }, false, "Usage: `goto [label]`\nStops current label and runs the given one. Stores the current position, which can be returned to using `return`");
-        AddMethodHook("set", (data) => {
-            if (properties.ContainsKey(data[1])) {
-                properties[data[1]] = data[2];
-            } else {
-                properties.Add(data[1], data[2]);
-            }
-        }, "Usage: `set [value] [amount]`\nSets the given value to [amount]. If it doesn't exist it is created first.`");
-        AddMethodHook("return", (data) => {
-            if (callStack.Count > 0) {
-                var pos = callStack.Pop();
-                RunLabel(pos.label, pos.line);
-            }
-        }, "Usage: `return`\nReturns to the last time execution was jumped, from an `if` or `goto`. Used for running some code, then continuing where you left off.");
-        AddMethodHook("include", (data) => {
-            Include(File.ReadAllLines(data[1]));
-        }, "Usage: `include [path_to_script]`\nAdds all the labels from another script into this script, so they can be called. Code outside of a label is not run automatically. Make sure the labels in the included script don't already exist in the current one.");
-        AddMethodHook("random", (data) => {
-            float min = float.Parse(data[2]);
-            float max = float.Parse(data[3]);
-            var random = Random.Range(min, max);
-            if (properties.ContainsKey(data[1])) {
-                properties[data[1]] = random.ToString();
-            } else {
-                properties.Add(data[1], random.ToString());
-            }
-        }, "Usage: `random [value] [min] [max]`\nGenerates a random value between min and max, and stores it in [value]. Both min and max are inclusive, meaning they can be generated.");
+
+        // Script Control
+
+        AddMethodHook(
+            "end", 
+            () => {}, 
+            false, 
+            "Stops execution! Scripts will already stop when they run out of commands but this can be used to add clarity or add a breakpoint."
+        );
+        
+        AddMethodHook(
+            "goto", 
+            (data) => {
+                callStack.Push(currentPosition);
+                RunLabel(data[1]);
+            }, 
+            false, 
+            "Stops current label and runs the given one. Stores the current position, which can be returned to using `return`",
+            LisaXParameter.Label
+        );
+
+        AddMethodHook(
+            "return", 
+            (data) => {
+                if (callStack.Count > 0) {
+                    var pos = callStack.Pop();
+                    RunLabel(pos.label, pos.line);
+                }
+            }, 
+            "Returns to the last time execution was jumped, from an `if` or `goto`. Used for running some code, then continuing where you left off."
+        );
+
+        AddMethodHook(
+            "include", 
+            (data) => {
+                Include(File.ReadAllLines(data[1]));
+            }, 
+            true, 
+            "Adds all the labels from another script into this script, so they can be called. Code outside of a label is not run automatically. Make sure the labels in the included script don't already exist in the current one.",
+            LisaXParameter.String
+        );
+        
+        // Mathematics
+
+        AddMethodHook("set", 
+            (data) => {
+                if (properties.ContainsKey(data[1])) {
+                    properties[data[1]] = data[2];
+                } else {
+                    properties.Add(data[1], data[2]);
+                }
+            }, 
+            true, 
+            "Usage: `set [value] [amount]`\nSets the given value to [amount]. If it doesn't exist it is created first.`",
+            LisaXParameter.Variable, LisaXParameter.Value
+        );
+        
+        AddMethodHook(
+            "add", 
+            (data) => {
+                if (properties.ContainsKey(data[1])) {
+                    properties[data[1]] = (float.Parse(properties[data[1]]) + float.Parse(data[2])).ToString();
+                } else {
+                    properties.Add(data[1], float.Parse(data[2]).ToString());
+                }
+            }, 
+            true,
+            "Adds amount to value. If value doesn't exist, it is created and set to [amount].",
+            LisaXParameter.Variable, LisaXParameter.Float
+        );
+        
+        AddMethodHook(
+            "sub", 
+            (data) => {
+                if (properties.ContainsKey(data[1])) {
+                    properties[data[1]] = (float.Parse(properties[data[1]]) - float.Parse(data[2])).ToString();
+                } else {
+                    properties.Add(data[1], (-float.Parse(data[2])).ToString());
+                }
+            },
+            "Subtracts amount from value. If value doesn't exist, it is created and set to -[amount].",
+            LisaXParameter.Variable, LisaXParameter.Float
+        );
+        
+        AddMethodHook(
+            "mul", 
+            (data) => {
+                properties[data[1]] = (float.Parse(properties[data[1]]) * float.Parse(data[2])).ToString();
+            }, 
+            "Multiplies a variable by a given amount",
+            LisaXParameter.Variable, LisaXParameter.Float
+        );
+        
+        AddMethodHook(
+            "div", 
+            (data) => {
+                properties[data[1]] = (float.Parse(properties[data[1]]) / float.Parse(data[2])).ToString();
+            }, 
+            "Divide a varible by a given amount",
+            LisaXParameter.Variable, LisaXParameter.Float
+        );
+        
+        AddMethodHook(
+            "pow", 
+            (data) => {
+                properties[data[1]] = Mathf.Pow(float.Parse(properties[data[1]]), float.Parse(data[2])).ToString();
+            }, 
+            "powerful",
+            LisaXParameter.Variable, LisaXParameter.Float
+        );
+        
+        AddMethodHook(
+            "random", 
+            (data) => {
+                float min = float.Parse(data[2]);
+                float max = float.Parse(data[3]);
+                var random = Random.Range(min, max);
+                if (properties.ContainsKey(data[1])) {
+                    properties[data[1]] = random.ToString();
+                } else {
+                    properties.Add(data[1], random.ToString());
+                }
+            }, 
+            "Generates a random value between two values, and stores it in a given value. Both min and max are inclusive.",
+            LisaXParameter.Variable, LisaXParameter.Float, LisaXParameter.Float
+        );
+
+        AddMethodHook(
+            "sine", 
+            (data) => {
+                float output = Mathf.Sin(float.Parse(data[2]) * Mathf.Deg2Rad );
+                if (properties.ContainsKey(data[1])) {
+                    properties[data[1]] = output.ToString();
+                } else {
+                    properties.Add(data[1], output.ToString());
+                }
+            }, 
+            "Calculates sin for the given angle in degrees and stores it in `[value]`",
+            LisaXParameter.Variable, LisaXParameter.Float
+        );
+
+        AddMethodHook(
+            "floor", 
+            (data) => {
+                properties[data[1]] = Mathf.Floor(float.Parse(properties[data[1]])).ToString();
+            },
+            "Rounds off a value to the next lowest whole number. e.g 1.9 becomes 1.0",
+            LisaXParameter.Variable
+        );
     }
 
-    public void AddMethodHook(string methodName, System.Action<string[]> method, string description)
+
+    public void AddMethodHook(string methodName, System.Action method, string description, params LisaXParameter[] parameters)
     {
-        AddMethodHook(methodName, method, true, description);
+        AddMethodHook(methodName, method, true, description, parameters);
     }
-
-    public void AddMethodHook(string methodName, System.Action<string[]> method, bool continues = true, string description = "")
-    {
-        methodHooks.Add(methodName, new LisaXMethodHook() {
-            name = methodName,
-            method = method,
-            continues = continues,
-            description = description
-        });
-    }
-
-    public void AddMethodHook(string methodName, System.Action method, string description)
-    {
-        AddMethodHook(methodName, method, true, description);
-    }
-
-    public void AddMethodHook(string methodName, System.Action method, bool continues = true, string description = "")
+    public void AddMethodHook(string methodName, System.Action method, bool continues = true, string description = "", params LisaXParameter[] parameters)
     {
         methodHooks.Add(methodName, new LisaXMethodHook() {
             name = methodName,
             method = (data) => { method.Invoke(); },
             continues = continues,
-            description = description
+            description = description,
+            parameters = parameters
+        });
+    }
+    public void AddMethodHook(string methodName, System.Action<string[]> method, string description, params LisaXParameter[] parameters)
+    {
+        AddMethodHook(methodName, method, true, description, parameters);
+    }
+    public void AddMethodHook(string methodName, System.Action<string[]> method, bool continues = true, string description = "", params LisaXParameter[] parameters)
+    {
+        methodHooks.Add(methodName, new LisaXMethodHook() {
+            name = methodName,
+            method = method,
+            continues = continues,
+            description = description,
+            parameters = parameters
         });
     }
 
@@ -246,7 +340,7 @@ public class LisaX
     public bool RunLine(string line)
     {
         if (line.Length == 0) return true;
-        string[] data = ParseTokens(line);
+        string[] data = ParseTokens(line.Trim(' '));
 
         if (data[0] == "if") {
 
@@ -262,16 +356,70 @@ public class LisaX
                 return true;
             }
         
+        } else if (data[0] == "ifgreater") {
+
+            if (properties.ContainsKey(data[1])) {
+
+                if (float.Parse(properties[data[1]]) > float.Parse(data[2])) {
+                    callStack.Push(currentPosition);
+                    RunLabel(data[3]);
+                    return false;
+                }
+
+            } else {
+                return true;
+            }
+        
         } else if (methodHooks.ContainsKey(data[0])) {
+            
+            if (data.Length != methodHooks[data[0]].parameters.Length + 1) {
+                WriteError(currentPosition.label, currentPosition.line, data, $"Incorrect number of arguments for method {data[0]}. Expected {methodHooks[data[0]].parameters.Length} things but got {data.Length - 1} !! :(");
+                return false;
+            }
+
+            for (int i = 0; i < data.Length-1; i++) {
+                string numbered = i.ToString() + "th";
+                if (i == 1) numbered = "1st";
+                if (i == 2) numbered = "2nd";
+                if (i == 3) numbered = "3rd";
+                switch (methodHooks[data[0]].parameters[i]) {
+                    case LisaXParameter.Float:
+                        if (!float.TryParse(data[i+1], out float _)) {
+                            WriteError(currentPosition.label, currentPosition.line, data, $"Hey i was trying to run this line of code and kind of expected the {numbered} parameter to be a number but it wasn't! Can you double check that you wrote the right thing? Not sure how to deal with {data[i+1]}.");
+                            return false;
+                        }
+                        break;
+                    case LisaXParameter.Label:
+                        if (!script.ContainsKey(data[1])) {
+                            WriteError(currentPosition.label, currentPosition.line, data, $"I was looking at the {numbered} parameter, hoping it'd be a label, but it wasn't! At least, I can't find a label called {data[i+1]}.");
+                            return false;
+                        }
+                        break;
+                }
+            }
 
             methodHooks[data[0]].method.Invoke(data);
             return methodHooks[data[0]].continues;
 
         } else {
             Debug.LogWarning($"Unknown command {line}");
+            WriteError(currentPosition.label, currentPosition.line, data, $"Unknown command {line}");
         }
 
         return true;
+    }
+
+    public void WriteError(string label, int line, string[] tokens, string error)
+    {
+        string currError;
+        if (File.Exists(errorFilePath))
+        {
+            currError = File.ReadAllText(errorFilePath);
+        } else {
+            currError = "";
+        }
+        currError += $"[{label} {line}]\n{string.Join(" ",tokens)}\n{error}\n";
+        File.WriteAllText(errorFilePath, currError);
     }
 
     public static float GetFloatValue(string value)
